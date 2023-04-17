a0f9843934a82ad7b564c5cfe56329502231623c
Macj04 <miguelcoa49@gmail.com>
2023-04-17 07:13:01 -0700

    Creating files for structure of this project

---
+++ test/main.h
@@ -0,0 +1,16 @@
+#ifndef SIMPLESHELL_H
+#define SIMPLESHELL_H
+
+/**Libreries*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/**Prototypes*/
+
+
+#endif
---
+++ test/shell.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * main - Function that creates a command line interpreter
+ * Return: Always 0
+ */
+int main(void)
+{
+
+}
